400e812fa156cde987dcd4a682c716699839f9cf
Carlos Alves <carlosalves369m@hotmail.com>
2022-08-08 09:41:10 -0300

    :hammer: update SQLs

---
+++ SQL_basic/state.sql
@@ -0,0 +1,40 @@
+CREATE TABLE "states" (
+     "id_state" int PRIMARY KEY NOT NULL,
+     "abbreviation" varchar(2) NOT NULL CHECK ("abbreviation" <> ''),
+     "state_value" varchar(60) NOT NULL CHECK ("state_value" <> '')
+);
+
+INSERT INTO "states" VALUES
+(1, 'AC', 'Acre'),
+(2, 'AL', 'Alagoas'),
+(3, 'AP', 'Amapá'),
+(4, 'AM', 'Amazonas'),
+(5, 'BA', 'Bahia'),
+(6, 'CE', 'Ceará'),
+(7, 'DF', 'Distrito Federal'),
+(8, 'ES', 'Espírito Santo'),
+(9, 'GO', 'Goiás'),
+(10, 'MA', 'Maranhão'),
+(11, 'MT', 'Mato Grosso'),
+(12, 'MS', 'Mato Grosso do Sul'),
+(13, 'MG', 'Minas Gerais'),
+(14, 'PA', 'Pará'),
+(15, 'PB', 'Paraíba'),
+(16, 'PR', 'Paraná'),
+(17, 'PE', 'Pernambuco'),
+(18, 'PI', 'Piauí'),
+(19, 'RJ', 'Rio de Janeiro'),
+(20, 'RN', 'Rio Grande do Norte'),
+(21, 'RS', 'Rio Grande do Sul'),
+(22, 'RO', 'Rondônia'),
+(23, 'RR', 'Roraima'),
+(24, 'SC', 'Santa Catarina'),
+(25, 'SP', 'São Paulo'),
+(26, 'SE', 'Sergipe'),
+(27, 'TO', 'Tocantins');
+
+
+SELECT COUNT(*) AS quant_estados from states
+
+SELECT abbreviation, state_value from states
+ORDER BY abbreviation ASC
